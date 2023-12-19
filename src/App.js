85d6a308b3fd42ed7c8c974edc9f9dda7106605f
Aadhya Dia <diaaadhya@gmai.com>
2023-12-19 18:49:36 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-//import FacultyUploadPage from "./components/FacultyUploadPage/FacultyUploadPage";
-import UploadData from './components/UploadData/UploadData';
+import FacultyUploadPage from "./components/FacultyUploadPage/FacultyUploadPage";
+//import UploadData from './components/UploadData/UploadData';
 function App() {
   return (
     <div className="App">
-      <UploadData />
+      <FacultyUploadPage />
     </div>
   );
 }
